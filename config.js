1ebf4bed9641f650c57ca5879b7ee673554f8e79
Donne431 <rofet343@gmail.com>
2025-12-28 16:14:33 +0300

    0.5.7

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var server_name = "Donne431";
-var server_ip = "Website 0.5.6";
-var server_port = "2022-2025";
+var server_ip = "Website 0.5.7";
+var server_port = "2022-2026";
 var ip_copied = "IP copied!";
 
 var firefly_count = 20;
